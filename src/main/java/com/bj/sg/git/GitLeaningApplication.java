5ebd6d9ae927bbd909d118fbd7beee901fa8c910
1248658532 <1248658532@qq.com>
2018-08-29 14:22:43 +0800

    git test

---
+++ src/main/java/com/bj/sg/git/GitLeaningApplication.java
@@ -0,0 +1,12 @@
+package com.bj.sg.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLeaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLeaningApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bj/sg/git/GitLeaningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bj.sg.git;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitLeaningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
