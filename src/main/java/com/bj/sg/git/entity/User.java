9ad5a39a006f106f127638223f4b85a6c4be27b5
1248658532 <1248658532@qq.com>
2018-08-29 15:15:38 +0800

    user entity

---
+++ src/main/java/com/bj/sg/git/entity/User.java
@@ -0,0 +1,30 @@
+package com.bj.sg.git.entity;
+
+public class User {
+	private String userName;
+	private Integer userAge;
+	private String userPassword;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Integer getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(Integer userAge) {
+		this.userAge = userAge;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", userAge=" + userAge + ", userPassword=" + userPassword + "]";
+	}
+	
+}
